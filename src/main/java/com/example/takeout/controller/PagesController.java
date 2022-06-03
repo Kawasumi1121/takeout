c6e1f3ac32d622664580320a7184d6c9f7a17697
kawasumi <wqgxy387@yahoo.co.jp>
2022-06-03 09:38:07 +0900

    layout #1

--- src/main/java/com/example/takeout/controller/PagesController.java
+++ src/main/java/com/example/takeout/controller/PagesController.java
@@ -1,4 +1,4 @@
-package com.example.pictgram.controller;
+package com.example.takeout.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
