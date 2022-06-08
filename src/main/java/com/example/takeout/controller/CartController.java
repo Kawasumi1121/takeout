061b5b5329bc416c64f012a389cce7d2a503f7c0
kawasumi <wqgxy387@yahoo.co.jp>
2022-06-08 16:55:02 +0900

    correct-1 #3

--- src/main/java/com/example/takeout/controller/CartController.java
+++ src/main/java/com/example/takeout/controller/CartController.java
@@ -1,12 +1,12 @@
 package com.example.takeout.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CartController {
 
-	@RequestMapping("/cart")
+	@GetMapping(path = "/cart")
 	public String index() {
 		return "cart/index";
 	}
